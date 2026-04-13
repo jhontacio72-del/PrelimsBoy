6c271a0825e01c34023a8417c263364ab403fffc
bct201-02 <bct201-02@DESKTOP-36VOQHQ>
2026-04-13 11:48:44 +0800

    4/13/26

---
+++ PrelimsBoy/Admin.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace PrelimsBoy
+{
+    partial class Admin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl_fill = new System.Windows.Forms.Panel();
+            this.pnl_side = new System.Windows.Forms.Panel();
+            this.pnl_Grades = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_grades = new System.Windows.Forms.Panel();
+            this.btn_enrollment = new System.Windows.Forms.Panel();
+            this.btn_billing = new System.Windows.Forms.Panel();
+            this.btn_myclasses = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_profile = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_logout = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pnl_fill.SuspendLayout();
+            this.pnl_side.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.btn_grades.SuspendLayout();
+            this.btn_enrollment.SuspendLayout();
+            this.btn_billing.SuspendLayout();
+            this.btn_myclasses.SuspendLayout();
+            this.btn_profile.SuspendLayout();
+            this.btn_logout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl_fill
+            // 
+            this.pnl_fill.Controls.Add(this.pnl_Grades);
+            this.pnl_fill.Controls.Add(this.pnl_side);
+            this.pnl_fill.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_fill.Location = new System.Drawing.Point(0, 0);
+            this.pnl_fill.Name = "pnl_fill";
+            this.pnl_fill.Size = new System.Drawing.Size(800, 450);
+            this.pnl_fill.TabIndex = 0;
+            // 
+            // pnl_side
+            // 
+            this.pnl_side.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.pnl_side.Controls.Add(this.btn_logout);
+            this.pnl_side.Controls.Add(this.btn_profile);
+            this.pnl_side.Controls.Add(this.btn_myclasses);
+            this.pnl_side.Controls.Add(this.btn_billing);
+            this.pnl_side.Controls.Add(this.btn_enrollment);
+            this.pnl_side.Controls.Add(this.btn_grades);
+            this.pnl_side.Controls.Add(this.label1);
+            this.pnl_side.Controls.Add(this.pictureBox1);
+            this.pnl_side.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnl_side.Location = new System.Drawing.Point(0, 0);
+            this.pnl_side.Name = "pnl_side";
+            this.pnl_side.Size = new System.Drawing.Size(200, 450);
+            this.pnl_side.TabIndex = 0;
+            // 
+            // pnl_Grades
+            // 
+            this.pnl_Grades.BackColor = System.Drawing.Color.Gray;
+            this.pnl_Grades.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_Grades.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pnl_Grades.Location = new System.Drawing.Point(200, 0);
+            this.pnl_Grades.Name = "pnl_Grades";
+            this.pnl_Grades.Size = new System.Drawing.Size(600, 450);
+            this.pnl_Grades.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(182, 113);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Admin";
+            // 
+            // btn_grades
+            // 
+            this.btn_grades.BackColor = System.Drawing.Color.SlateGray;
+            this.btn_grades.Controls.Add(this.label2);
+            this.btn_grades.Location = new System.Drawing.Point(3, 163);
+            this.btn_grades.Name = "btn_grades";
+            this.btn_grades.Size = new System.Drawing.Size(194, 32);
+            this.btn_grades.TabIndex = 2;
+            // 
+            // btn_enrollment
+            // 
+            this.btn_enrollment.BackColor = System.Drawing.Color.SlateGray;
+            this.btn_enrollment.Controls.Add(this.label3);
+            this.btn_enrollment.Location = new System.Drawing.Point(3, 201);
+            this.btn_enrollment.Name = "btn_enrollment";
+            this.btn_enrollment.Size = new System.Drawing.Size(194, 32);
+            this.btn_enrollment.TabIndex = 3;
+            // 
+            // btn_billing
+            // 
+            this.btn_billing.BackColor = System.Drawing.Color.SlateGray;
+            this.btn_billing.Controls.Add(this.label4);
+            this.btn_billing.Location = new System.Drawing.Point(3, 239);
+            this.btn_billing.Name = "btn_billing";
+            this.btn_billing.Size = new System.Drawing.Size(194, 32);
+            this.btn_billing.TabIndex = 3;
+            // 
+            // btn_myclasses
+            // 
+            this.btn_myclasses.BackColor = System.Drawing.Color.SlateGray;
+            this.btn_myclasses.Controls.Add(this.label5);
+            this.btn_myclasses.Location = new System.Drawing.Point(3, 277);
+            this.btn_myclasses.Name = "btn_myclasses";
+            this.btn_myclasses.Size = new System.Drawing.Size(194, 32);
+            this.btn_myclasses.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(60, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Grades";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(41, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Enrollment";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(60, 4);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 25);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Billing";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(45, 2);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 25);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "My Classes";
+            // 
+            // btn_profile
+            // 
+            this.btn_profile.BackColor = System.Drawing.Color.SlateGray;
+            this.btn_profile.Controls.Add(this.label6);
+            this.btn_profile.Location = new System.Drawing.Point(3, 315);
+            this.btn_profile.Name = "btn_profile";
+            this.btn_profile.Size = new System.Drawing.Size(194, 32);
+            this.btn_profile.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(60, 2);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 25);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Profile";
+            // 
+            // btn_logout
+            // 
+            this.btn_logout.BackColor = System.Drawing.Color.SlateGray;
+            this.btn_logout.Controls.Add(this.label7);
+            this.btn_logout.Location = new System.Drawing.Point(3, 353);
+            this.btn_logout.Name = "btn_logout";
+            this.btn_logout.Size = new System.Drawing.Size(194, 32);
+            this.btn_logout.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(60, 2);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(84, 25);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Log Out";
+            // 
+            // Admin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnl_fill);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Admin";
+            this.Text = "Admin";
+            this.pnl_fill.ResumeLayout(false);
+            this.pnl_side.ResumeLayout(false);
+            this.pnl_side.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.btn_grades.ResumeLayout(false);
+            this.btn_grades.PerformLayout();
+            this.btn_enrollment.ResumeLayout(false);
+            this.btn_enrollment.PerformLayout();
+            this.btn_billing.ResumeLayout(false);
+            this.btn_billing.PerformLayout();
+            this.btn_myclasses.ResumeLayout(false);
+            this.btn_myclasses.PerformLayout();
+            this.btn_profile.ResumeLayout(false);
+            this.btn_profile.PerformLayout();
+            this.btn_logout.ResumeLayout(false);
+            this.btn_logout.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_fill;
+        private System.Windows.Forms.Panel pnl_Grades;
+        private System.Windows.Forms.Panel pnl_side;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel btn_myclasses;
+        private System.Windows.Forms.Panel btn_billing;
+        private System.Windows.Forms.Panel btn_enrollment;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel btn_grades;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel btn_logout;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel btn_profile;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
